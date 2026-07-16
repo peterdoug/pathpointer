e97ebf4cf266ad3c39a8b0fb5c3876482afb1c68
Peter Dougherty <peterdoug96@gmail.com>
2026-07-16 09:37:46 +0200

    Refresh cache for max deviation feature

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pathpointer-v5";
+const CACHE = "pathpointer-v6";
 const CORE = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
